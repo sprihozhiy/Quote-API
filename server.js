ce4c6a0aa144619953154cb944c3fc4db2e600d1
sprihozhiy <sprihozhiy@gmail.com>
2021-04-04 17:31:50 -0600

    added random quote req/res

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
 
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = {};
+  randomQuote.quote = getRandomElement(quotes);
+  //   console.log(randomQuote);
+  res.status(200).send(randomQuote);
+});
+
 app.listen(PORT, () => {
   console.log(`Application is running on port ${PORT}`);
 });
