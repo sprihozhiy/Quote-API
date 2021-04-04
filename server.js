3510db380ae09975a3676176e38f1a6f959de6f7
sprihozhiy <sprihozhiy@gmail.com>
2021-04-04 17:54:47 -0600

    added req/res for getting all quotes

--- server.js
+++ server.js
@@ -8,11 +8,24 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static("public"));
 
+//Getting a random quote
 app.get("/api/quotes/random", (req, res, next) => {
   const randomQuote = {};
   randomQuote.quote = getRandomElement(quotes);
   //   console.log(randomQuote);
   res.status(200).send(randomQuote);
+});
+
+//Getting all quotes
+app.get("/api/quotes", (req, res, next) => {
+  const emptyQuery = Object.keys(req.query).length;
+  if (emptyQuery === 0) {
+    const allQuotes = {};
+    allQuotes.quotes = [...quotes];
+    res.status(200).send(allQuotes);
+  } else {
+    console.log("Nothing works :-(");
+  }
 });
 
 app.listen(PORT, () => {
