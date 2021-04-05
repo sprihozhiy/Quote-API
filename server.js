29daf919c3e2ae98fa18e5d5a2aa5544e59297eb
sprihozhiy <sprihozhiy@gmail.com>
2021-04-04 19:25:15 -0600

    added retrieving of all or author qotes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const { quotes } = require("./data");
-const { getRandomElement } = require("./utils");
+const { getRandomElement, checkTheAuthor } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
@@ -16,15 +16,18 @@ app.get("/api/quotes/random", (req, res, next) => {
   res.status(200).send(randomQuote);
 });
 
-//Getting all quotes
+//Getting all quotes and author's quotes
 app.get("/api/quotes", (req, res, next) => {
   const emptyQuery = Object.keys(req.query).length;
+  const retrievedQuotes = {};
   if (emptyQuery === 0) {
-    const allQuotes = {};
-    allQuotes.quotes = [...quotes];
-    res.status(200).send(allQuotes);
+    retrievedQuotes.quotes = [...quotes];
+    res.status(200).send(retrievedQuotes);
   } else {
-    console.log("Nothing works :-(");
+    const person = req.query.person;
+    checkTheAuthor(quotes, person);
+    retrievedQuotes.quotes = checkTheAuthor(quotes, person);
+    res.status(200).send(retrievedQuotes);
   }
 });
 
--- utils.js
+++ utils.js
@@ -1,8 +1,19 @@
-const getRandomElement = arr => {
-  if (!Array.isArray(arr)) throw new Error('Expected an array');
+const getRandomElement = (arr) => {
+  if (!Array.isArray(arr)) throw new Error("Expected an array");
   return arr[Math.floor(Math.random() * arr.length)];
+};
+
+function checkTheAuthor(arr, item) {
+  const authorQuotes = [];
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].person === item) {
+      authorQuotes.push(arr[i]);
+    }
+  }
+  return authorQuotes;
 }
 
 module.exports = {
-  getRandomElement
+  getRandomElement,
+  checkTheAuthor,
 };
